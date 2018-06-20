a968e81a4bd8992daccd1e7d8add4b265f4741c4
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-20 10:05:11 +0000

    Imp

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ sense = SenseHat()
 class Application():
 	text = "_"
 	button_text = None
+	displaying = False
 	def __init__(self, master):
 		self.frame = Frame(master)
 		self.frame.pack()
@@ -16,7 +17,7 @@ class Application():
 							text="QUIT",
 							fg="red",
 							bg="blue",
-							command=self.frame.quit)
+							command=self.quit_app)
 		self.button.pack(side=LEFT)
 		
 		self.button_text = Button(self.frame,
@@ -33,19 +34,29 @@ class Application():
 		back_colour=[0,0,255])
 		
 	def text_anzeigen(self):
+		tkMessageBox.showinfo("Anzeige", "Der gewuenschte Text wird nun ausgegeben.")
+		self.displaying = True
 		self.button_text.config(state='disabled')
 		sense.show_message(self.text, 
 		scroll_speed=0.05,
 		text_colour=[255,255,0],
 		back_colour=[0,0,255])
 		self.button_text.config(state='normal')
+		self.displaying = False
 		
 	def ask_text(self):
-		self.text = tkSimpleDialog.askstring("Text", "Welcher Text soll angezeigt werden?")
-		if(self.text == None):
-			tkMessageBox.showwarning("Fehler", "Es wurde kein Text angegeben.")
+		if not self.displaying:
+			self.text = tkSimpleDialog.askstring("Text", "Welcher Text soll angezeigt werden?")
+			if(self.text == None):
+				tkMessageBox.showwarning("Fehler", "Es wurde kein Text angegeben.")
+			else:
+				self.text_anzeigen()
 		else:
-			self.text_anzeigen()
+			tkMessageBox.showerror("Fehler", "Es wird bereits etwas angezeigt.")
+
+	def quit_app(self):
+		tkMessageBox.showinfo("Vielen Dank!", "Vielen Dank für die Nutzung von senseUI. Die Anwendung wird nun geschlossen.")
+		self.frame.quit()
 
 		
 root = Tk()
