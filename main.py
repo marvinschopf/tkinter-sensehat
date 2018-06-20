7fb23d10057056d6ff76e3b18cf1f55d3b3128b9
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-20 09:56:43 +0000

    Improve gui

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from sense_hat import SenseHat
 from Tkinter import *
+import tkSimpleDialog
+import tkMessageBox
 sense = SenseHat()
 
 class Application():
+	text = "_"
+	button_text = None
 	def __init__(self, master):
 		self.frame = Frame(master)
 		self.frame.pack()
@@ -15,18 +19,33 @@ class Application():
 							command=self.frame.quit)
 		self.button.pack(side=LEFT)
 		
-		self.button_hello = Button(self.frame,
-									text="Hello",
+		self.button_text = Button(self.frame,
+									text="Text",
 									fg="green",
 									bg="cyan",
-									command=self.sense_hello)
-		self.button_hello.pack(side=LEFT)
+									command=self.ask_text)
+		self.button_text.pack(side=LEFT)
 	
 	def sense_hello(self):
 		sense.show_message("Hello", 
 		scroll_speed=0.05,
 		text_colour=[255,255,0],
 		back_colour=[0,0,255])
+		
+	def text_anzeigen(self):
+		self.button_text.config(state='disabled')
+		sense.show_message(self.text, 
+		scroll_speed=0.05,
+		text_colour=[255,255,0],
+		back_colour=[0,0,255])
+		self.button_text.config(state='normal')
+		
+	def ask_text(self):
+		self.text = tkSimpleDialog.askstring("Text", "Welcher Text soll angezeigt werden?")
+		if(self.text == None):
+			tkMessageBox.showwarning("Fehler", "Es wurde kein Text angegeben.")
+		else:
+			self.text_anzeigen()
 
 		
 root = Tk()
